ebdf2a7ec42924d4410a4cd0e23c8f40b862e93e
bilin li <bilin.li@mercedes-benz.com>
2024-10-06 01:38:53 +0200

    update environment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afa19f243a4034fbcab4e1a1be0fb6c7-494097186.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
